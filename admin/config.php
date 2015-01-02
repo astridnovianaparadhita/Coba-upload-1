aea460dcafc1d7bf4ce492bb667f872226b49e37
astrid noviana p <astrid_chan@ymail.com>
2015-01-02 12:43:51 +0700

    Online pertama v1

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Online/admin/');
+define('HTTP_CATALOG', 'http://localhost/Online/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Online/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Online/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\Online/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\Online/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\Online/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\Online/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\Online/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\Online/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\Online/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\Online/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\Online/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\Online/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\Online/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Online');
+define('DB_PREFIX', 'oc_');
+?>
